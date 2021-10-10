05a194b2d989aa725b36caaadd6b2be8ac85e706
IlkaMilka123 <92274406+IlkaMilka123@users.noreply.github.com>
2021-10-10 23:47:08 +0300

    Update 321.lua

--- 321.lua
+++ 321.lua
@@ -1,4 +1,4 @@
 for _,v in pairs(game.Players:GetPlayers()) do
 if v:IsA("Player") then
-v:Kick("you were banned for cheating for: 999999999999 days")
+v:Kick("you are banned for cheats for: 999999999 days ")
 end end
